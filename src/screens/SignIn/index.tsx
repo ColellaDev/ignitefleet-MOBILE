8335ce23bc1097878dd7e9ea69fa5e89a56549a8
Marcos Colella <colella.dev@gmail.com>
2024-09-17 12:08:22 -0300

    Componente Button

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import { Container, Title, Slogan } from './styles';
 import backgroundImg from '../../assets/background.png'
 
+import { Button } from '../../components/Button';
+
 export function SignIn() {
   return (
     <Container source={backgroundImg}>
@@ -9,6 +11,8 @@ export function SignIn() {
       <Slogan>
         Gestão de uso de veículos
       </Slogan>
+
+      <Button title='Entrar com Google' />
     </Container>
   );
 }
