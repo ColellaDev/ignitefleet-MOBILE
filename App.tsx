a38104a0b317795861e92fc4b86ef43282772cd1
Marcos Colella <colella.dev@gmail.com>
2024-09-17 11:44:51 -0300

    Fontes personalizadas + Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
     <ThemeProvider theme={theme}>
-    <SignIn />
+       <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent 
+      />
+      <SignIn />
     </ThemeProvider>
   );
 }
