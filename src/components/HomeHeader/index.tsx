02662da88f8ad311e56fcb2022260ac8a8475fdf
Marcos Colella <colella.dev@gmail.com>
2024-09-23 17:42:24 -0300

    Instalando React-Native-SVG e Phosphor Icon / Adicionado Icone Logout

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native'
+import { Power } from 'phosphor-react-native'
 import { Container, Greeting, Message, Name } from './styles';
+
+import theme from '../../theme';
+
 export function HomeHeader() {
   return (
     <Container>
@@ -11,6 +16,11 @@ export function HomeHeader() {
           Rodrigo
         </Name>
       </Greeting>
+
+      <TouchableOpacity>
+        <Power size={32} color={theme.COLORS.GRAY_400} />
+      </TouchableOpacity>
+      
     </Container>
   );
 }
