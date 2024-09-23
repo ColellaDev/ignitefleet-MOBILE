6743dba87b0386a61a9e2193564b8095733f155f
Marcos Colella <colella.dev@gmail.com>
2024-09-23 17:36:20 -0300

    Criando o Header

---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Greeting, Message, Name } from './styles';
+export function HomeHeader() {
+  return (
+    <Container>
+      <Greeting>
+        <Message>
+          Olá
+        </Message>
+        <Name>
+          Rodrigo
+        </Name>
+      </Greeting>
+    </Container>
+  );
+}
